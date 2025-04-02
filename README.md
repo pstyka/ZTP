efadc6e5345c7a921edb0111af65f88bdd25e3fc
Pawel Styka <pawel@styka.pl>
2025-04-02 18:50:31 +0200

    Update - Init RestAPI

--- README.md
+++
@@ -1,1 +0,0 @@
-# ZTP - Zaawansowane techniki programowania
---
+++ REST_API/src/test/java/org/example/pawel/PawelApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.pawel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PawelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
