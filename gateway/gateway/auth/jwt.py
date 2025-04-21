ee6390aa0752e5217eccdbe4cb8e3f9dc92a1aa7
GrzegorzPustulka <94792342+GrzegorzPustulka@users.noreply.github.com>
2025-04-22 00:01:09 +0200

    users: endpoint /users/me (#6)

--- gateway/gateway/auth/jwt.py
+++ gateway/gateway/auth/jwt.py
@@ -3,7 +3,7 @@ import logging
 from jose import JWTError, jwt
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
-
+from dataclasses import dataclass
 from gateway.core.config import settings
 
 logger = logging.getLogger("gateway.auth.jwt")
@@ -11,10 +11,10 @@ logger = logging.getLogger("gateway.auth.jwt")
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/token")
 
 
+@dataclass
 class TokenData:
-    def __init__(self, username: str | None = None, role: str | None = None):
-        self.username = username
-        self.role = role
+    username: str
+    role: str
 
 
 async def verify_token(token: str = Depends(oauth2_scheme)) -> TokenData:
@@ -28,7 +28,7 @@ async def verify_token(token: str = Depends(oauth2_scheme)) -> TokenData:
         payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
         username: str = payload.get("sub")
 
-        if username is None:
+        if not username:
             logger.warning("Token missing username")
             raise credentials_exception
 
@@ -38,15 +38,3 @@ async def verify_token(token: str = Depends(oauth2_scheme)) -> TokenData:
         logger.warning(f"JWT error: {str(e)}")
         raise credentials_exception
 
-
-def has_role(role: str):
-    async def role_checker(token_data: TokenData = Depends(verify_token)) -> TokenData:
-        if role == token_data.role:
-            return token_data
-
-        raise HTTPException(
-            status_code=status.HTTP_403_FORBIDDEN,
-            detail="Not enough permissions"
-        )
-
-    return role_checker
