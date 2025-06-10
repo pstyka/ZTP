ef25b8e28200f0f25580e9abba740aa95ab13231
pawel <pawel@styka.pl>
2025-06-10 23:37:23 +0200

    RestAPI: added pagination to getFlats. deleted trash.

--- REST_API/src/main/java/org/example/pawel/controller/FlatController.java
+++ REST_API/src/main/java/org/example/pawel/controller/FlatController.java
@@ -8,7 +8,10 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import org.example.pawel.dto.FlatDTO;
 import org.example.pawel.service.FlatPhotosService;
 import org.example.pawel.service.FlatService;
+import org.springframework.data.domain.Page;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -25,33 +28,39 @@ public class FlatController {
     @Autowired
     private FlatService flatService;
 
-    @Autowired
-    private FlatPhotosService flatPhotosService;
-
     @Operation(summary = "Get all flats", description = "Returns a list of all flats available in the system.")
-    @GetMapping
+    @GetMapping()
     public List<FlatDTO> getAllFlats() {
         return flatService.getAllFlats();
+    }
+
+    @Operation(summary = "Get all flats with pagination", description = "Returns a paginated list of all flats available in the system.")
+    @GetMapping("/paginated")
+    public Page<FlatDTO> getAllFlatsPaginated(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size) {
+        Pageable pageable = PageRequest.of(page, size);
+        return flatService.getAllFlatsPaginated(pageable);
     }
 
     @Operation(summary = "Search flats", description = "Search flats based on filter criteria: city, rooms, min/max price, availability, and area.")
     @GetMapping("/search")
     public List<FlatDTO> searchFlats(
-            @RequestParam(required = false) String city,
-            @RequestParam(required = false) Integer rooms,
-            @RequestParam(required = false) Double minPrice,
-            @RequestParam(required = false) Double maxPrice,
-            @RequestParam(required = false) Boolean isAvailable,
-            @RequestParam(required = false) Double minArea,
-            @RequestParam(required = false) Double maxArea
+        @RequestParam(required = false) String city,
+        @RequestParam(required = false) Integer rooms,
+        @RequestParam(required = false) Double minPrice,
+        @RequestParam(required = false) Double maxPrice,
+        @RequestParam(required = false) Boolean isAvailable,
+        @RequestParam(required = false) Double minArea,
+        @RequestParam(required = false) Double maxArea
     ) {
         return flatService.searchFlats(city, rooms, minPrice, maxPrice, isAvailable, minArea, maxArea);
     }
 
     @Operation(summary = "Create a new flat", description = "Adds a new flat to the system.")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "201", description = "Flat successfully created"),
-            @ApiResponse(responseCode = "400", description = "Invalid input data")
+        @ApiResponse(responseCode = "201", description = "Flat successfully created"),
+        @ApiResponse(responseCode = "400", description = "Invalid input data")
     })
     @PostMapping
     public ResponseEntity<UUID> createFlat(@RequestBody FlatDTO flatDTO) {
@@ -59,10 +68,15 @@ public class FlatController {
         return ResponseEntity.status(HttpStatus.CREATED).body(flatId);
     }
 
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "201", description = "Flat with photos successfully created"),
+        @ApiResponse(responseCode = "400", description = "Invalid input data")
+    })
+    @Operation(summary = "Create a new flat with photos", description = "Adds a new flat with associated photos to the system.")
     @PostMapping("/with-photos")
     public ResponseEntity<UUID> createFlatWithPhotos(
-            @RequestPart("flat") FlatDTO flatDTO,
-            @RequestPart("photos") List<MultipartFile> photos) {
+        @RequestPart("flat") FlatDTO flatDTO,
+        @RequestPart("photos") List<MultipartFile> photos) {
 
         UUID flatId = flatService.addFlatWithPhotos(flatDTO, photos);
         return ResponseEntity.status(HttpStatus.CREATED).body(flatId);
@@ -70,8 +84,8 @@ public class FlatController {
 
     @Operation(summary = "Delete a flat", description = "Deletes a flat by its ID.")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "204", description = "Flat successfully deleted"),
-            @ApiResponse(responseCode = "404", description = "Flat not found with the given ID")
+        @ApiResponse(responseCode = "204", description = "Flat successfully deleted"),
+        @ApiResponse(responseCode = "404", description = "Flat not found with the given ID")
     })
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteFlat(@PathVariable UUID id) {
@@ -81,8 +95,8 @@ public class FlatController {
 
     @Operation(summary = "Update a flat", description = "Updates the details of a flat using its ID.")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Flat successfully updated"),
-            @ApiResponse(responseCode = "404", description = "Flat not found with the given ID")
+        @ApiResponse(responseCode = "200", description = "Flat successfully updated"),
+        @ApiResponse(responseCode = "404", description = "Flat not found with the given ID")
     })
     @PutMapping("/{id}")
     public ResponseEntity<FlatDTO> updateFlat(@PathVariable UUID id, @RequestBody FlatDTO flatDTO) {
@@ -99,7 +113,7 @@ public class FlatController {
     @Operation(summary = "Get flat by ownerID")
     @GetMapping("/owner/{ownerId}")
     public ResponseEntity<List<FlatDTO>> getFlatsByOwnerId(
-            @Parameter(description = "Owner ID to filter flats by owner") @PathVariable UUID ownerId) {
+        @Parameter(description = "Owner ID to filter flats by owner") @PathVariable UUID ownerId) {
         List<FlatDTO> flats = flatService.getFlatsByOwnerId(ownerId);
         return ResponseEntity.ok(flats);
     }
--- REST_API/src/main/java/org/example/pawel/controller/FlatPhotosController.java
+++ REST_API/src/main/java/org/example/pawel/controller/FlatPhotosController.java
@@ -1,6 +1,7 @@
 package org.example.pawel.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.example.pawel.service.FlatPhotosService;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +14,7 @@ import java.util.UUID;
 @RestController
 @RequestMapping("/rest/flats")
 @RequiredArgsConstructor
+@Tag(name = "Flat Photos", description = "Operations related to managing photos of rental flats")
 public class FlatPhotosController {
 
     private final FlatPhotosService flatPhotoService;
